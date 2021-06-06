9ca01fb97944cc0489be9991e44c3e26b6aaf702
Deiman999202 <43722510+Deiman999202@users.noreply.github.com>
2021-06-06 11:27:38 +0300

    Delete 07 homework directory

--- 07 homework/js/task-01.js
+++
@@ -1,7 +0,0 @@
-const categoriesEl = document.querySelectorAll(".item");
-console.log(`В списке ${categoriesEl.length} категории`);
-
-const categoriesNamesAndElements = categoriesEl.forEach(category => {
-    console.log(category.firstElementChild.textContent);
-    console.log(`Колличество елементов: ${category.querySelectorAll('li').length}`)
-})
